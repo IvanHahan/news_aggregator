69b8f95db2a93cf08554e3bce66ef4a093770f94
Ivan Hahanov <ivanhahanov@gmail.com>
2025-09-16 15:32:14 +0300

    Load environment variables at startup for configuration management

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import time
 
+from dotenv import load_dotenv
+
 from content_maker import ContentMaker
 
 if __name__ == "__main__":
+    load_dotenv()
     content_maker = ContentMaker.build()
     while True:
         content_maker.run()
