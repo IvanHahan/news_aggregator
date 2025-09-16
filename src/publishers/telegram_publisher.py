e624ee7c259a98cb51858e40ad28bb277b301eb1
Ivan Hahanov <ivanhahanov@gmail.com>
2025-09-16 14:05:50 +0300

    Refactor TelegramPublisher to initialize TelegramClient in the constructor and streamline message publishing to all channels.

--- src/publishers/telegram_publisher.py
+++ src/publishers/telegram_publisher.py
@@ -9,16 +9,19 @@ class TelegramPublisher(BasePublisher):
     def __init__(
         self, api_id: int, api_hash: str, telegram_bot_token: str, channels: list[str]
     ):
-        self.client = TelegramClient("telegram_publisher", api_id, api_hash)
         self.channels = channels
         self.telegram_bot_token = telegram_bot_token
+        self.api_id = api_id
+        self.api_hash = api_hash
 
     def publish(self, content: str) -> None:
-        for channel in self.channels:
-            asyncio.run(self._publish_to_channel(channel, content))
+        asyncio.run(self._publish_to_all_channels(content))
 
-    async def _publish_to_channel(self, channel: str, content: str) -> None:
-        async with self.client:
-            await self.client.send_message(
-                await self.client.get_input_entity(channel), content
-            )
+    async def _publish_to_all_channels(self, content: str) -> None:
+        async with TelegramClient(
+            "telegram_publisher", self.api_id, self.api_hash
+        ) as client:
+            for channel in self.channels:
+                await client.send_message(
+                    await client.get_input_entity(channel), content
+                )
