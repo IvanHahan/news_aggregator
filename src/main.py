ae90998240783797ea2cba7bd5c071b2a5ba412a
Ivan Hahanov <ivanhahanov@gmail.com>
2025-09-16 16:51:48 +0300

    Remove infinite loop in main.py, allowing ContentMaker.run to execute once for improved execution flow

--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-import time
-
 from dotenv import load_dotenv
 
 from content_maker import ContentMaker
@@ -7,6 +5,4 @@ from content_maker import ContentMaker
 if __name__ == "__main__":
     load_dotenv()
     content_maker = ContentMaker.build()
-    while True:
-        content_maker.run()
-        time.sleep(60 * 60)  # Sleep for 30 minutes (1800 seconds)
+    content_maker.run()
