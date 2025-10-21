b06871f40d5ba6e95288b860d3af62824c9cd3f5
Ivan Hahanov <ivanhahanov@gmail.com>
2025-10-21 17:01:05 +0300

    Fix import statement for PromptTemplate in link_explorer.py

--- src/link_explorer.py
+++ src/link_explorer.py
@@ -35,8 +35,8 @@ from urllib.parse import urlparse
 
 import requests
 from bs4 import BeautifulSoup
-from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
 
 from data_model import LinkContent
 
